a614470f6abbdcd67c150eae3412bab5d98004f5
ishida-ryuga <r.ishida@infomart.co.jp>
2026-02-18 17:30:42 +0900

    コントローラーにリクエストを送信する

---
+++ src/main/resources/static/js/memo.js
@@ -0,0 +1,15 @@
+function post (){
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const from = document.getElementById("from");
+    const formData = new FormData(form);
+    const XHR =new XMLHttpRequest();
+    XHR.open("post", "/posts",true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+  });
+};
+
+
+window.addEventListener('load', post);
